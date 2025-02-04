dcac426d55844acccb46d32a80f03c26a3257c3f
Andrey B <72738126+Andrey-B-lab@users.noreply.github.com>
2025-02-04 13:32:26 +0200

    Update networking.tf

--- networking.tf
+++ networking.tf
@@ -1,25 +1,24 @@
-# Security Group for the second server
-resource "aws_security_group" "allow_all_from_my_ip" {
-  name        = "allow_all_from_my_ip"
-  description = "Allow all from my IP"
-  vpc_id      = data.aws_vpc.default.id
-  
+resource "aws_security_group" "my_ip_ssh" {
+  name        = "sg-my-ip-ssh"
+  description = "Allow SSH from my IP"
+  vpc_id      = var.vpc_id
+
   tags = {
-    Name = "my_ec2_sg-myIP"
+    Name = "sg-my-ip-ssh"
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "example_ingress" {
-  security_group_id = aws_security_group.example.id
-  description       = "Allow SSH from my IP"
+resource "aws_vpc_security_group_ingress_rule" "my_ip_ssh_ingress" {
+  security_group_id = aws_security_group.my_ip_ssh.id
+  description       = "SSH from my IP"
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  cidr_blocks       = ["${var.my_ip_address}/32"]  # If you want to restrict to exactly one IP
+  cidr_blocks       = ["${var.my_ip_address}/32"]
 }
 
-resource "aws_vpc_security_group_egress_rule" "example_egress" {
-  security_group_id = aws_security_group.example.id
+resource "aws_vpc_security_group_egress_rule" "my_ip_ssh_egress" {
+  security_group_id = aws_security_group.my_ip_ssh.id
   description       = "Allow all outbound"
   from_port         = 0
   to_port           = 0
