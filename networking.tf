cb2c52a675e8d1507adf9cdb7d95bc6f5682167e
Andrey B <72738126+Andrey-B-lab@users.noreply.github.com>
2025-02-04 12:01:15 +0200

    Update networking.tf

--- networking.tf
+++ networking.tf
@@ -1,31 +1,41 @@
-resource "aws_security_group" "allow_all_from_my_ip" {
-  name        = "allow_all_from_my_ip"
-  description = "Allow inbound from my IP for all ports"
-  vpc_id      = "YOUR-VPC-ID" # If you're using the default VPC, you can omit or data-source it.
-
+# Security Group for the second server
+resource "aws_security_group" "jenkins_access" {
+  name        = "jenkins_access"
+  description = "Allow SSH from Jenkins server"
+  vpc_id      = data.aws_vpc.default.id
+  
   tags = {
-    Name = "allow_all_from_my_ip"
+    Name = "my_ec2_sg-jenkins"
   }
 }
 
-# Ingress rule: Allow all protocols/ports from my IP
-resource "aws_security_group_rule" "ingress_all_from_my_ip" {
-  type              = "ingress"
-  security_group_id = aws_security_group.allow_all_from_my_ip.id
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["${var.my_ip_address}/32"]
-  description       = "Allow all inbound from my IP"
+# Ingress rule for IPv4
+resource "aws_vpc_security_group_ingress_rule" "ssh_from_jenkins_ipv4" {
+  security_group_id = aws_security_group.jenkins_access.id
+  description       = "Allow SSH from Jenkins IPv4"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.jenkins_ip_address}/32"]  # The Jenkins server's public IP or private IP range
 }
 
-# Egress rule: Allow all outbound
-resource "aws_security_group_rule" "egress_all_outbound" {
-  type              = "egress"
-  security_group_id = aws_security_group.allow_all_from_my_ip.id
+# Optionally, if you actually need IPv6
+# If not needed, omit this entirely
+resource "aws_vpc_security_group_ingress_rule" "ssh_from_jenkins_ipv6" {
+  security_group_id    = aws_security_group.jenkins_access.id
+  description          = "Allow SSH from Jenkins IPv6"
+  from_port            = 22
+  to_port              = 22
+  protocol             = "tcp"
+  ipv6_cidr_blocks     = ["${var.jenkins_ip_address}/128"]  # If you have a real IPv6
+}
+
+# Egress rule: allow all outbound (common default)
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
+  security_group_id = aws_security_group.jenkins_access.id
+  description       = "Allow all outbound"
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
-  description       = "Allow all outbound"
 }
